053e12a9b90c6957607a9b72363b5b719b537891
David Ferguson <davidferguson@users.noreply.github.com>
2019-09-08 21:24:32 +0100

    initial commit

---
+++ lektor_mathshistory_jinja.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from lektor.pluginsystem import Plugin
+import time
+import os
+
+class MathshistoryJinjaPlugin(Plugin):
+    name = 'mathshistory-jinja'
+    description = u'Defines several useful jinja2 filters and globals that are used throughout the Maths History site.'
+
+    def on_setup_env(self, **extra):
+
+        def refs_format(blocks):
+            res = []
+            for block in blocks:
+                res.append({
+                    'number': block['number'],
+                    'reference': block['reference']
+                })
+            return res
+
+        def basename(path):
+            return os.path.basename(path)
+
+        def without_ext(name):
+            return os.path.splitext(name)[0]
+
+        self.env.jinja_env.filters['refs_format'] = refs_format
+        self.env.jinja_env.filters['basename'] = basename
+        self.env.jinja_env.filters['without_ext'] = without_ext
