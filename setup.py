647d2e7ee6736622abd8ce5dd9c7edb473cbd800
David Ferguson <davidferguson@users.noreply.github.com>
2019-09-08 22:28:56 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,38 @@
+import ast
+import io
+import re
+
+from setuptools import setup, find_packages
+
+with io.open('README.md', 'rt', encoding="utf8") as f:
+    readme = f.read()
+
+_description_re = re.compile(r'description\s+=\s+(?P<description>.*)')
+
+with open('lektor_mathshistory_daily.py', 'rb') as f:
+    description = str(ast.literal_eval(_description_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    author='David Ferguson',
+    author_email='fergusondavid6@gmail.com',
+    description=description,
+    keywords='Lektor plugin maths history site',
+    license='MIT',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    name='lektor-mathshistory-daily',
+    packages=find_packages(),
+    py_modules=['lektor_mathshistory_daily'],
+    url='https://github.com/mathshistory/mathshistory-daily',
+    version='0.1.0',
+    classifiers=[
+        'Framework :: Lektor',
+        'Environment :: Plugins',
+    ],
+    entry_points={
+        'lektor.plugins': [
+            'mathshistory-daily = lektor_mathshistory_daily:MathshistoryDailyPlugin',
+        ]
+    }
+)
