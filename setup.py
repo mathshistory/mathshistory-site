937226815f5ec256c37accf4810a6a20afa832a3
David Ferguson <davidferguson@users.noreply.github.com>
2019-09-10 11:54:20 +0100

    release 0.1.1
    - changes map function to list comprehension fixing json serialise error
    - changes underscore to dash in PLUGIN_NAME, fixing plugin not found error

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     packages=find_packages(),
     py_modules=['lektor_mathshistory_categoryindex'],
     url='https://github.com/mathshistory/mathshistory-categoryindex',
-    version='0.1.0',
+    version='0.1.1',
     classifiers=[
         'Framework :: Lektor',
         'Environment :: Plugins',
