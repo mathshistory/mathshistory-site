2c468141290346736eb0058408cba54a909f825d
David Ferguson <davidferguson@users.noreply.github.com>
2019-09-13 00:19:04 +0100

    increment version to 1.x

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     packages=find_packages(),
     py_modules=['lektor_mathshistory_chronology'],
     url='https://github.com/mathshistory/mathshistory-chronology',
-    version='0.1.0',
+    version='1.0.0',
     classifiers=[
         'Framework :: Lektor',
         'Environment :: Plugins',
