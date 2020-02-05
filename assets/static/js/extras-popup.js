262a44d0380de9bd1ab5caf1459b0e7eed10fc1e
David Ferguson <davidferguson@users.noreply.github.com>
2020-02-05 08:36:47 +0000

    fix extras popup

--- assets/static/js/extras-popup.js
+++ assets/static/js/extras-popup.js
@@ -28,11 +28,12 @@ document.body.onfocus = function () {
   }
 }
 
-// go through them all
-var elinks = document.getElementsByClassName('elink')
-for (var i = 0; i < elinks.length; i++) {
-  var elink = elinks[i]
-  elink.onclick = function (e) {
+// go through all links, processing ones that go to /Extras/
+var links = document.getElementsByTagName('a')
+for (var i = 0; i < links.length; i++) {
+  var link = links[i]
+  if (link.href.indexOf('/Extras/') === -1) continue
+  link.onclick = function (e) {
     var href = e.target.href
     extraLinkPopup(href)
     e.preventDefault()
