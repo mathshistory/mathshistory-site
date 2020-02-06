ebcc14160c6288061956a04094c6617473d1c80c
David Ferguson <davidferguson@users.noreply.github.com>
2020-02-06 22:36:07 +0000

    fix gaz link, and also biography links

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     packages=find_packages(),
     py_modules=['lektor_mathshistory_map'],
     url='https://github.com/mathshistory/mathshistory-map',
-    version='0.1.1',
+    version='0.2.0',
     classifiers=[
         'Framework :: Lektor',
         'Environment :: Plugins',
