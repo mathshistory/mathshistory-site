f39268e37ca21b43ca24c67400d00a98ab089659
David Ferguson <davidferguson@users.noreply.github.com>
2020-02-07 12:33:34 +0000

    use canonical jinja filter to get absolute url

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     packages=find_packages(),
     py_modules=['lektor_mathshistory_posters'],
     url='https://github.com/mathshistory/mathshistory-posters',
-    version='0.1.0',
+    version='1.0.0',
     classifiers=[
         'Framework :: Lektor',
         'Environment :: Plugins',
