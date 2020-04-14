96d3e46177216141427e11503ac554bc4da34b9c
David Ferguson <davidferguson@users.noreply.github.com>
2020-04-14 10:28:55 +0100

    implement search, fixes #106

---
+++ templates/plugins/searchconfig.php
@@ -0,0 +1,25 @@
+<?php
+  $stream_opts = array(
+    'http' => array(
+      'proxy'           => '138.251.14.57:8080',
+      'request_fulluri' => true,
+    ),
+  );
+
+  //$baseUrl = "https://l-funadmin.st-andrews.ac.uk/s/search.html?";
+  $baseUrl = "https://t-funadmin.st-andrews.ac.uk/s/search.html?";
+  $mode = "";
+  $query = "";
+  $collection = "uofsa-web-maths-history";
+  //$profile = "_default";
+  $profile = "_default_preview";
+  $form = "resultsonly";
+  $theme = "";
+  $browse = "theme";
+  $audience = "";
+  $discipline = "";
+  $serviceID = "";
+  $queryStringForForm = "";
+  $startRank = "";
+  $resultsPerPage = "10";
+?>
