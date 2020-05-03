a32aa1e766f5412bd91ab7e65e698b97bd7983b9
David Ferguson <davidferguson@users.noreply.github.com>
2020-05-03 02:09:48 +0100

    bump readme

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=['beautifulsoup4','html5lib','regex'],
     py_modules=['lektor_mathshistory_renderer'],
     url='https://github.com/mathshistory/mathshistory-renderer',
-    version='0.4.2',
+    version='0.4.3',
     classifiers=[
         'Framework :: Lektor',
         'Environment :: Plugins',
