4f1261052899f11f54856e4e597b045fae51d44b
David Ferguson <davidferguson@users.noreply.github.com>
2020-05-26 09:07:47 +0100

    bump version

--- packages/mathshistory-daily/setup.py
+++ packages/mathshistory-daily/setup.py
@@ -25,7 +25,7 @@ setup(
     packages=find_packages(),
     py_modules=['lektor_mathshistory_daily'],
     url='https://github.com/mathshistory/mathshistory-daily',
-    version='0.4.0',
+    version='0.4.1',
     classifiers=[
         'Framework :: Lektor',
         'Environment :: Plugins',
